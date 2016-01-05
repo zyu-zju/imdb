bb35cdf9b0e356a6d718603441c842833911cb28
zyu-zju <billyyuzhihua@gmail.com>
2016-01-05 15:07:57 +0800

    Entry Point of MAIN
    template to create main

---
+++ imdb.py
@@ -0,0 +1,10 @@
+"""Script to gather IMDB keywords from 2015's top grossing movies"""
+
+import sys
+
+def main():
+	"""Main entry point for the script"""
+	pass
+
+if __name__ == '__main__':
+	sys.exit(main())
