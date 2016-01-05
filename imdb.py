902c52927c7df2dbdd2e5c3c172d82fe52404128
zyu-zju <billyyuzhihua@gmail.com>
2016-01-05 15:14:39 +0800

    with stubs

--- imdb.py
+++ imdb.py
@@ -2,8 +2,19 @@
 
 import sys
 
+URL = 'http://www.imdb.com/search/title?at=0&sort=boxoffice_gross_us,desc&start=1&year=2015,2015'
+
 def main():
 	"""Main entry point for the script"""
+	pass
+
+def get_top_grossing_movie_links(url):
+	"""Return a list of tuples containing the top grossing movies of 2015 and link to their IMDB
+	page."""
+	pass
+
+def get_keywords_for_move(url):
+	"""Return a list of keywords associated with *movie*"""
 	pass
 
 if __name__ == '__main__':
